9d5b9485b6c7d593e9495469275ff1d2c7520d58
Daan [xx] <xangadix@gmail.com>
2018-07-31 15:45:46 +0200

    updating the readme

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
-
 app.get('/', (req, res) => res.send('Hello World!'))
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+/*
+  each Source:
+    check existance
+      - init
+      - update
+      - render
+    check interface
+      - set src 
+      - play
+      - pause
+      - currentTime
+      - currentFrame
+      - duration
+      - load source
