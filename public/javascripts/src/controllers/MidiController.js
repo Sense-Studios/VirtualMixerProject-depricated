b9877caf4c24c81c613f6937ea16860d619aa992
xangadix <xangadix@Mellon.localdomain>
2018-08-24 16:55:10 +0200

    merge #2

--- public/javascripts/src/controllers/MidiController.js
+++ public/javascripts/src/controllers/MidiController.js
@@ -1,5 +1,6 @@
 
-// based on https://gist.github.com/xangadix/936ae1925ff690f8eb430014ba5bc65e
+// refers to ...
+// https://gist.github.com/xangadix/936ae1925ff690f8eb430014ba5bc65e
 
 function MidiController( renderer, _mixer1, _mixer2, _mixer3 ) {
   // returns a floating point between 1 and 0, in sync with a bpm
