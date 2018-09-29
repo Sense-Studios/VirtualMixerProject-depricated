4e10db30f4aef240aae4eab3aaa6bdd0b1c28597
xangadix <xangadix@Mellon.localdomain>
2018-09-29 02:37:17 +0200

    WIP We have behaviours ...!

---
+++ routes/proxy.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+
+
+
+/* GET proxy listing. */
+router.get('/1', function(req, res, next) {
+
+  //var newurl = 'http://37.220.36.53:7904';
+  var newurl = 'http://93.90.201.81:8000';
+  request(newurl).pipe(res);
+
+  /*
+
+  var request = require('request');
+  app.get('/', function(req,res) {
+    //modify the url in any way you want
+    var newurl = 'http://google.com/';
+    request(newurl).pipe(res);
+  });
+
+  */
+
+  //res.send('respond with a proxy');
+
+
+});
+
+router.get('/nsb', function(req, res, next) {
+  request('http://37.220.36.53:7904').pipe(res);
+});
+
+router.get('/dunklenacht', function(req, res, next) {
+  request('http://93.90.201.81:8000').pipe(res);
+});
+
+
+module.exports = router;
