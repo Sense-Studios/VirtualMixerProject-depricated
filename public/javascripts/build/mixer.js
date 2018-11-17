5583a8c93b5fa2fd155a94875c5e0a7911d342d2
Daan [xx] <xangadix@gmail.com>
2018-11-17 14:02:22 +0100

    stashing local changes

--- public/javascripts/build/mixer.js
+++ public/javascripts/build/mixer.js
@@ -191,7 +191,6 @@ function AudioAnalysis( renderer ) {
   }
 }
 
-<<<<<<< HEAD
 /**
  * @description
  *   BPM (Audio analysis)
@@ -207,8 +206,42 @@ function AudioAnalysis( renderer ) {
  */
 
 function BPM( renderer, options ) {
-  // returns a floating point between 1 and 0, in sync with a bpm
+
   var _self = this
+
+  _self.function_list = [
+    ["AUTO", "method", "toggleAutoBpm"],
+    ["MODDOWN", "method", "modDown"],
+    ["MODUP", "method", "modUp"],
+    ["MOD", "method", "modNum"]
+  ]
+
+  // define scheme for this Addon?
+  /*
+  _self.scheme = function() {
+    var scheme = {
+      description: {
+        "name": "BPM",
+        "type": "Addon"
+      },
+      inputs: {
+        "bypass": "Boolean",
+        "audio": "Addon",
+        "mod": "number"
+      },
+      outputs: {
+        "bpm": "number",
+        "bpm_float": "float"
+      }
+    }
+    return scheme;
+  }
+  */
+
+  if ( renderer == undefined ) return
+  // returns a floating point between 1 and 0, in sync with a bpm
+
+
 
   // exposed variables.
   _self.uuid = "BPM_" + (((1+Math.random())*0x100000000)|0).toString(16).substring(1);
@@ -266,15 +299,23 @@ function BPM( renderer, options ) {
    * @description Audio analysis
    * @member Addon#BPM#useAutoBpm#
    * @member Addon#BPM#autoBpmData#
-   * @member Addon#BPM#useMicrophone
+   * @member Addon#BPM#tempodata_bpm#
    * @member Addon#BPM#audio_src
+   * @member Addon#BPM#useMicrophone
    */
-  _self.useAutoBpm = true      // auto bpm
+  _self.useAutoBpm = false      // auto bpm
+  _self.tempodata_bpm = 128     // from music
   _self.mute = false
   _self.autoBpmData = {}       // info object for the auto bpm
-  _self.useMicrophone = false  // use useMicrophone for autoBPM
+
   _self.audio_src = ""         // audio file or stream (useMicrophone = false)
+
+  // TODO
+  _self.useMicrophone = false  // use useMicrophone for autoBPM
+
+  // DEPRICATED
   _self.bypass = false
+
 
   // source.renderer ?
   var nodes = []
@@ -285,27 +326,7 @@ function BPM( renderer, options ) {
   // add to renderer
   renderer.add(_self)
 
-  // define scheme for this Addon
-  /*
-  _self.scheme = function() {
-    var scheme = {
-      description: {
-        "name": "BPM",
-        "type": "Addon"
-      },
-      inputs: {
-        "bypass": "Boolean",
-        "audio": "Addon",
-        "mod": "number"
-      },
-      outputs: {
-        "bpm": "number",
-        "bpm_float": "float"
-      }
-    }
-    return scheme;
-  }
-  */
+
 
   // init with a tap contoller
   _self.init = function() {
@@ -318,9 +339,17 @@ function BPM( renderer, options ) {
   // UPDATE
   var starttime = (new Date()).getTime()
   _self.update = function() {
-    nodes.forEach( function( node ) {
-      if ( _self.useAutoBpm ) node( _self.render() );
-    });
+
+    // rename useAnalyser?
+    if ( _self.useAutoBpm ) {
+      _self.bpm = _self.tempodata_bpm
+    }
+
+    if ( !_self.disabled ) {
+      nodes.forEach( function( node ) {
+        node( _self.render() );
+      });
+    }
 
     c = ((new Date()).getTime() - starttime) / 1000;
     _self.sec = c * Math.PI * (_self.bpm * _self.mod) / 60            // * _self.mod
@@ -349,8 +378,13 @@ function BPM( renderer, options ) {
   _self.toggleAutoBpm = function( _num ) {
     _self.useAutoBpm  = !_self.useAutoBpm
     console.log("--->", _self.useAutoBpm  )
-
   }
+
+  _self.turnOff = function() {
+    bpm.audio.muted = false
+    bpm.useAutoBpm = false
+  }
+
   // add nodes, implicit
   _self.add = function( _func ) {
     nodes.push( _func )
@@ -483,8 +517,228 @@ function BPM( renderer, options ) {
     if ( _self.bypass ) return;
     // if  no src && no mic -- return
     // if ... -- return
-=======
->>>>>>> c724b46a34df04da238b5a3f159f0bf9b5b21e67
+
+    var dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray)
+
+    // precalculculations, push only the highest value of the frequency range
+    var now = Date.now()
+    var high = 0
+    dataArray.forEach( (d) => { if ( d > high ) high = d })
+    dataSet.push( [ now, high ] )
+
+    // keep the set on sampleLength
+    if (dataSet.length > sampleLength) dataSet.splice(0, dataSet.length - sampleLength)
+    d++
+
+    // SLOWPOKE
+    // take a snapshot every 1/10 second and calculate beat
+    if ( ( now - start) > 100 ) {
+
+      var tempoData = getTempo(dataSet)
+
+      // Here are some ideas for a more complete analisis range
+
+      // var tempoCounts = tempoData.tempoCounts
+      // getBlackout // TODO -- detects blackout, prolonged, relative silence in sets
+      // getAmbience // TODO -- detects overal 'business' of the sound, it's ambience
+
+      // drawData(dataSet) // DEPRICATED -- draw the wavelines (for testing)
+
+      // depricated failsafe
+      // if (tempoCounts[0] !== undefined) window.bpm = tempoCounts[0].tempo
+
+      // depricated debug window
+      // $('#info').html( dataSet.length + "\t " + c * 10 + " samples/s" + "\t peaks: "  + foundpeaks.length + "\tBPM: <strong>"+ Math.round(window.bpm) + " </strong> ("+Math.round(window.bpm2)+") \t\tconfidence: <em>" + window.confidence + " <strong>" + window.calibrating + "</strong></em>" ) //.+ " -- " + _dataSet[ _dataSet.length - 1 ] )
+      // console.log(" ### AUTOBPM: ",  window.calibrating, tempoData.bpm, d, tempoData.foundpeaks.length, treshold )
+
+      // write the test data globally (needs uiid?)
+      _self.tempodata_bpm = tempoData.bpm
+      if ( _self.useAutoBPM ) _self.sec = c * Math.PI * (tempoData.bpm * _self.mod) / 60
+      start = Date.now()
+      d = 0
+    }
+  }
+
+  // blink on the beat with element with class .blink
+  var doBlink = function() {
+    if ( document.getElementsByClassName('blink').length == 0 ) return
+    if ( audio.paused ) {
+      document.getElementsByClassName('blink')[0].style.opacity = 0
+    }else{
+      if (document.getElementsByClassName('blink')[0].style.opacity == 1) {
+        document.getElementsByClassName('blink')[0].style.opacity = 0
+      }else{
+        document.getElementsByClassName('blink')[0].style.opacity = 1
+      }
+    }
+    setTimeout( doBlink, (60/ (_self.bpm) )*1000 / _self.mod    )
+  }
+  doBlink()
+
+  // get bpm tempo by analyising audio stream
+  var getTempo = function( _data ) {
+    foundpeaks = []                    // reset foundpeaks
+    peaks = new Array( _data.length )  // reset peaks
+
+    // find peaks
+    for ( var i = 0; i < _data.length; i++ ) {
+      if ( _data[i] !== undefined && _data[i][1] > ( treshold * 128 ) ) {
+        peaks[i] = [ dataSet[i][0], 1 ];           // update in peaks
+        foundpeaks.push( [ _data[i][0], 1 ] );     // add to foundpeaks
+        i += 50;                                   // += 0.4s, to move past this peak
+      }
+    }
+
+    // make sure we have enough peaks by adjusting the treshhold
+    if ( foundpeaks.length < peak_min ) treshold -= 0.05;
+    if ( foundpeaks.length > peak_max ) treshold += 0.05;
+    if ( treshold > 2 ) treshold = 2;
+    if ( treshold < 1 ) treshold = 1;
+
+    // calculate tempo by grouping peaks and calculate interval between them
+    // see: http://joesul.li/van/beat-detection-using-web-audio/
+    // for more information on this method and the sources of the algroritm
+    var tempoCounts = groupNeighborsByTempo( countIntervalsBetweenNearbyPeaks( foundpeaks ) );
+    tempoCounts.sort( mycomparator );                             // sort tempo's by 'score', or most neighbours
+    if ( tempoCounts.length == 0 ) {
+      tempoCounts[0] = { tempo: 0 }; // if no temp is found, return 0
+    }else{
+
+
+      // DISPLAY, for debugging, requires element with an .info class
+      var html = ""
+      tempoCounts.reverse().forEach(function(v,i) {
+        html += i + ", " + v.tempo + ", " + v.count + "\t ["
+        var j = 0
+        while( j < v.count) {
+          html += '#'
+          j++
+        }
+        html += ']<br/>'
+      })
+      if (document.getElementById('info') != null) {
+        document.getElementById('info').html = html
+      }
+
+    }
+
+    // Callibration feedback (~24 seconids)
+    var confidence = "calibrating"
+    var calibrating = false
+    if ( _data[0] === undefined ) {
+      calibrating = true
+      if ( document.getElementsByClassName('blink').length > 0 ) document.getElementsByClassName('blink')[0].style.backgroundColor = '#999999';
+    }else{
+      calibrating = false
+
+      // race condition
+      if (tempoCounts[0] === undefined  || tempoCounts[1] === undefined ) {
+        console.log("holdit")
+        return
+      }
+
+      var confidence_mod = tempoCounts[0].count - tempoCounts[1].count
+      if ( confidence_mod <= 2 ) {
+        confidence = "low"
+        if ( document.getElementsByClassName('blink').length > 0 ) document.getElementsByClassName('blink')[0].style.backgroundColor = '#990000';
+      }else if( confidence_mod > 2 && confidence_mod <= 7) {
+        confidence = "average"
+        if ( document.getElementsByClassName('blink').length > 0 ) document.getElementsByClassName('blink')[0].style.backgroundColor = '#999900';
+      }else if( confidence_mod > 7 ) {
+        confidence = "high"
+        if ( document.getElementsByClassName('blink').length > 0 ) document.getElementsByClassName('blink')[0].style.backgroundColor = '#CCCCCC';
+      }
+    }
+
+    // return an object with all the necc. data.
+    var tempoData = {
+      bpm: tempoCounts[0].tempo,     // suggested bpm
+      confidence: confidence,        // String
+      calibrating: calibrating,      // ~24 seconds
+      treshold: treshold,            // current treshold
+      tempoCounts: tempoCounts,      // current tempoCounts
+      foundpeaks:  foundpeaks,       // current found peaks
+      peaks: peaks                   // all peaks, for display only
+    }
+
+    //console.log(tempoData.bpm, tempoData.confidence)
+
+    return tempoData;
+  }
+
+  // HELPERS
+  // sort helper
+  var mycomparator = function ( a,b ) {
+    return parseInt( a.count, 10 ) - parseInt( b.count, 10 );
+  }
+
+  // generate interval counter
+  var countIntervalsBetweenNearbyPeaks = function( _peaks ) {
+
+    // reset
+    intervalCounts = [];
+
+    _peaks.forEach(function(peak, index) {
+      for(var i = 0; i < 10; i++) {
+        if ( _peaks[index + i] !== undefined ) {
+          var interval = _peaks[index + i][0] - peak[0];
+          var foundInterval = intervalCounts.some( function(intervalCount) {
+            if (intervalCount.interval === interval) return intervalCount.count++;
+          });
+          if (!foundInterval) intervalCounts.push({ interval: interval, count: 1 });
+        }
+      }
+    });
+
+    return intervalCounts;
+  }
+
+  // group intervalcounts by temp
+  var groupNeighborsByTempo = function( intervalCounts ) {
+
+    // reset
+    var tempoCounts = []
+    var noTempo = false
+
+    // start the interval counts
+    intervalCounts.forEach(function(intervalCount, i) {
+
+      // Convert an interval to tempo
+      if (intervalCount.interval != 0 && !isNaN(intervalCount.interval)) {
+        var theoreticalTempo = 60 / (intervalCount.interval / 1000)
+      }
+
+      // Adjust the tempo to fit within the 90-180 BPM range
+      while (theoreticalTempo < 90) theoreticalTempo *= 2;
+      while (theoreticalTempo > 180) theoreticalTempo /= 2;
+
+      // round to 2 beat
+      theoreticalTempo = Math.round( theoreticalTempo/2 ) * 2
+
+      var foundTempo = tempoCounts.some(function(tempoCount) {
+        if (tempoCount.tempo === theoreticalTempo && !noTempo )
+          return tempoCount.count += intervalCount.count;
+      });
+
+      // add it to the tempoCounts
+      if (!foundTempo) {
+        if ( theoreticalTempo && !noTempo ) {
+          tempoCounts.push({
+            tempo: theoreticalTempo,
+            count: intervalCount.count
+          })
+        }
+      }
+    });
+
+    return tempoCounts
+  } // end groupNeighborsByTempo
+
+} // end BPM
+
+
+
 
 function FileManager( _source ) {
 
@@ -492,8 +746,8 @@ function FileManager( _source ) {
 
   try {
     renderer
-  } catch {
-    _self.function_list = [["CHANGE", "method", "changez"], ["POD", "set","pod"] ]
+  } catch(e) {
+    _self.function_list = [ ["CHANGE", "method", "changez"], ["POD", "set","pod"] ]
     return
   }
 
@@ -725,12 +979,9 @@ function Behaviour( renderer, options ) {
   _self.script = {}
   _self.sheets = []
   _self.sheet_index = 0
-<<<<<<< HEAD
-=======
 
   // requires a bpm
   _self.bpm = options.bpm
->>>>>>> c724b46a34df04da238b5a3f159f0bf9b5b21e67
 
   renderer.add(_self)
 
@@ -825,14 +1076,6 @@ function Behaviour( renderer, options ) {
   _self.checkSheets = function() {
      // _self.beats%_self.sheets[0].length
      var __beats = sheet_pointer%_self.sheets[ sheet_index ].length
-<<<<<<< HEAD
-    //console.log("check", sheet_pointer,  sheet_pointer%_self.sheets[0].length)
-    //if ( old_sheet_pointer != sheet_pointer ) {
-      //console.log( "Boem:", __beats, sheet_pointer, "sheets:", _self.sheets[0][sheet_pointer%_self.sheets[0].length] )
-      checkBeats(sheet_pointer%_self.sheets[ _self.sheet_index ].length)
-      old_sheet_pointer = sheet_pointer
-
-=======
     // console.log("check", sheet_pointer,  sheet_pointer%_self.sheets[0].length)
     // if ( old_sheet_pointer != sheet_pointer ) {
       // console.log( "Boem:", __beats, sheet_pointer, "sheets:", _self.sheets[0][sheet_pointer%_self.sheets[0].length] )
@@ -840,7 +1083,6 @@ function Behaviour( renderer, options ) {
       checkBeats(sheet_pointer%_self.sheets[ _self.sheet_index ].length)
 
 
->>>>>>> c724b46a34df04da238b5a3f159f0bf9b5b21e67
       _self.sheets[ _self.sheet_index ][sheet_pointer%_self.sheets[ _self.sheet_index ].length].forEach( function( trigger_pointer ) {
 
 
@@ -862,11 +1104,7 @@ function Behaviour( renderer, options ) {
               }else{
                   _args = trigger_pointer[2]  // BLEND //isnan?
               }
-<<<<<<< HEAD
-              
-=======
 
->>>>>>> c724b46a34df04da238b5a3f159f0bf9b5b21e67
               target[ _func[2] ](_args);
 
               console.log(target, _function, _args)
@@ -878,11 +1116,7 @@ function Behaviour( renderer, options ) {
       })
     //}
     sheet_pointer += 1
-<<<<<<< HEAD
-    setTimeout( _self.checkSheets, ((60/window.bpm_test)*1000)/4 )
-=======
     setTimeout( _self.checkSheets, ((60/bpm.bpm)*1000)/4 )
->>>>>>> c724b46a34df04da238b5a3f159f0bf9b5b21e67
   }
 
   var fireTrigger = function(trigger) {
